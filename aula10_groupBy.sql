a246a1c9894eb910b00a0c3f8a5c72b97a09dff8
Camila Alves <116912821+alvescamila87@users.noreply.github.com>
2023-09-21 17:10:23 -0300

    mysql

--- aula10_groupBy.sql
+++ aula10_groupBy.sql
@@ -13,3 +13,27 @@ group by nacionalidade;
 # agrupamento de registros + função de agregação 3
 select totaulas, count(*) from cursos
 group by totaulas;
+
+# agrupamento com where
+select carga, count(nome) from cursos where totaulas = '30'
+group by carga;
+
+# agrupando e agregando (having) 1
+select ano, count(ano) from cursos
+group by ano
+having count(ano) >= '5'
+order by count(*) desc;
+
+# agrupando e agregando (having) 2
+select ano, count(ano) from cursos
+group by ano
+having ano > '2016'
+order by count(*) desc;
+
+# juntas selects ()
+select avg(carga) from cursos;
+
+select carga, count(*) from cursos
+where ano > '2015'
+group by carga
+having carga > (select avg(carga) from cursos);
